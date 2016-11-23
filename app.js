cde9a62f43fcf597572a52ef7c368dba49f1700c
Andrej Remic <andrej.remic@gmail.com>
2016-11-23 12:24:30 +0100

    Preklop z WEBUI in prikaz stanja

--- app.js
+++ app.js
@@ -2,34 +2,45 @@ var express = require("express");
 var app = express();
 var gpio = require("gpio");
 
-app.get("/", function(req, res){
-	res.send("Hello world!");
-});
-
 var gpio203 = gpio.export(203, {
    direction: "out",
    ready: function() {
-   	console.log("GPIO 203 VALUE: ", gpio203.value);
+   console.log("GPIO 203 VALUE: ", gpio203.value);
    }
 });
 
+app.get("/", function(req, res){
+	if (gpio203.value == 1) {
+		res.send('<h2>Preklopnik 2Mbit linkov!</h2><a href="/1" style="background-color:red">PRIMARY</a><br><a href="/0">BACKUP</a>');
+	} else {
+		res.send('<h2>Preklopnik 2Mbit linkov!</h2><a href="/1">PRIMARY</a><br><a href="/0" style="background-color:red">BACKUP</a>');
+	};
+});
+
 app.get("/0", function(req, res){
-	res.send("Izbrali ste 0!");
 	gpio203.set(0);
 	setTimeout(function() {
-	console.log("GPIO 203 VALUE: ", gpio203.value);
-	}, 1000);
+		console.log("GPIO 203 VALUE: ", gpio203.value);
+		if (gpio203.value == 1) {
+			res.send('<h2>Preklopnik 2Mbit linkov!</h2><a href="/1" style="background-color:red">PRIMARY</a><br><a href="/0">BACKUP</a>');
+		} else {
+			res.send('<h2>Preklopnik 2Mbit linkov!</h2><a href="/1">PRIMARY</a><br><a href="/0" style="background-color:red">BACKUP</a>');
+		};
+	}, 100);
 });
 
 app.get("/1", function(req, res){
-	res.send("Izbrali ste 1!");
 	gpio203.set(1);
 	setTimeout(function() {
-	console.log("GPIO 203 VALUE: ", gpio203.value);
-	}, 1000);
+	    console.log("GPIO 203 VALUE: ", gpio203.value);
+		if (gpio203.value == 1) {
+			res.send('<h2>Preklopnik 2Mbit linkov!</h2><a href="/1" style="background-color:red">PRIMARY</a><br><a href="/0">BACKUP</a>');
+		} else {
+			res.send('<h2>Preklopnik 2Mbit linkov!</h2><a href="/1">PRIMARY</a><br><a href="/0" style="background-color:red">BACKUP</a>');
+		};
+	}, 100);
 });
 
-
-app.listen(80,function(){
+app.listen(80, function(){
 	console.log("Server is listening on port 80!!!");
 });
