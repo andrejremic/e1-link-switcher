2977c0595428fc01f66e2f07399195db72acea54
Andrej Remic <andrej.remic@gmail.com>
2016-11-23 08:00:02 +0100

    GPIO203 set 0/1

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 var express = require("express");
 var app = express();
+var gpio = require("gpio");
 
 app.get("/", function(req, res){
 	res.send("Hello world!");
 });
+
+var gpio203 = gpio.export(203, {
+   direction: "out",
+   ready: function() {
+   	console.log("GPIO 203 VALUE: ", gpio203.value);
+   }
+});
+
+app.get("/0", function(req, res){
+	res.send("Izbrali ste 0!");
+	gpio203.set(0);
+	setTimeout(function() {
+	console.log("GPIO 203 VALUE: ", gpio203.value);
+	}, 1000);
+});
+
+app.get("/1", function(req, res){
+	res.send("Izbrali ste 1!");
+	gpio203.set(1);
+	setTimeout(function() {
+	console.log("GPIO 203 VALUE: ", gpio203.value);
+	}, 1000);
+});
+
 
 app.listen(80,function(){
 	console.log("Server is listening on port 80!!!");
