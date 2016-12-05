ee972cef47e785a935a8dddd67e6e720eaa0a2c0
Andrej Remic <andrej.remic@gmail.com>
2016-12-05 13:50:54 +0100

    WEBUI: prikaze status, izboljsana povratna info., izgled

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var eth0IP = os.networkInterfaces().eth0[0].address;
 
 // CONFIGURATION
 var webuiPort = 80;
-var pin7preklop = new Gpio(203, 'high'); // export GPIO to userspace, export: gpio 203 (pin 7), direction: out, value: 1
+var pin7preklop = new Gpio(203, 'out'); // export GPIO to userspace, export: gpio 203 (pin 7), direction: out, value: 0
+var	statusPovezave;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -16,29 +17,36 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs'); // da ni potrebno pisat .ejs
 
 function mapAktivnaPovezava(val){
-	if (val===1) return 'backup';
-	if (val===0) return 'master';
-	if (val==='master') return 0;
-	if (val==='backup') return 1;
+	if (val===1) return 'REDUNDANTNA';
+	if (val===0) return 'PRIMARNA';
+	if (val==='PRIMARNA') return 0;
+	if (val==='REDUNDANTNA') return 1;
 };
-
 
 pin7preklop.read(function(err, value){
 	pin7preklop.stanjeIzhoda = value;
-	console.log('Trenutno aktivna povezava: '+mapAktivnaPovezava(value).toUpperCase());
+	statusPovezave = mapAktivnaPovezava(value);
+	console.log('Trenutno aktivna povezava: '+statusPovezave);
 });
 
+statusPovezave = mapAktivnaPovezava(pin7preklop.stanjeIzhoda);
+
 app.post('/preklop', function(req, res){
-	aktivnaPovezava = req.body.preklop;
-	console.log('REQUEST 2:['+parseInt(aktivnaPovezava)+']');
-	pin7preklop.write(parseInt(aktivnaPovezava));
-	res.send({success: 'true'});
+	aktivnaPovezava = parseInt(req.body.preklop);
+	pin7preklop.write(aktivnaPovezava, function(){
+		statusPovezave = mapAktivnaPovezava(aktivnaPovezava);
+		console.log('WEBUI preklop na: '+statusPovezave);
+		res.send({success: 'true'});
+	});
+});
+
+app.post('/status', function(req, res){
+	console.log('WEBUI POST /status: '+statusPovezave);
+	res.send({status: mapAktivnaPovezava(statusPovezave)});
 });
 
 app.get('/', function(req, res){
-	pin7preklop.read(function(err, value){
-		res.render('home', {aktivnaPovezava: value});
-	});
+	res.render('home');
 });
 
 
---
+++ public/main.js
@@ -0,0 +1,25 @@
+$.post( "/status", function(data){
+	console.log(data.status);
+	if (data.status===1){
+		$("#backupBtn").addClass("btn-danger").removeClass("btn-default");
+		$("#masterBtn").addClass("btn-default").removeClass("btn-success");
+	} else if (data.status===0) {
+		$("#masterBtn").addClass("btn-success").removeClass("btn-default");
+		$("#backupBtn").addClass("btn-default").removeClass("btn-danger");
+	}
+});
+
+
+$("#masterBtn").on('click', function(){
+	$.post( "/preklop", {preklop: 0}, function(){
+		$("#masterBtn").addClass("btn-success").removeClass("btn-default");
+		$("#backupBtn").addClass("btn-default").removeClass("btn-danger");
+	});
+});
+
+$("#backupBtn").on('click', function(){
+	$.post( "/preklop", {preklop: 1}, function(){
+		$("#backupBtn").addClass("btn-danger").removeClass("btn-default");
+		$("#masterBtn").addClass("btn-default").removeClass("btn-success");
+	});
+});
